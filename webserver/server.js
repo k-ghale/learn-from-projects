914555d1c0fcb96c268835531d6cc4c1cd0241d3
k-ghale <kabinghale888@gmail.com>
2026-01-14 20:39:16 -0500

    created a web server on NodeJS

---
+++ webserver/server.js
@@ -0,0 +1,30 @@
+import * as net from "net";
+
+
+function newConn(socket) {
+    console.log("new connection", socket.remoteAddress, socket.remotePort); 
+
+    server.on('end', () => {
+        console.log("EOG. ");
+    });
+
+    server.on( 'data', (data) => {
+        console.log("DATA :", data.toString());
+        socket.write(data);
+
+        if(data.includes("q")) {
+            console.log('closing');
+            socket.end();
+        }
+    });
+
+
+}
+
+let server = net.createServer();
+server.on('connection', newConn);
+server.on('err',  (err) => {throw err});
+
+server.listen({host: '127.0.0.1', port: 1234});
+
+
