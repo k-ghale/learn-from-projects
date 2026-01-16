6ec4bd3c051fe8cb602d805d89dfefbe760f2e9a
kabinghale <127985604+k-ghale@users.noreply.github.com>
2026-01-15 22:58:58 -0500

    Update server.js

--- webserver/server.js
+++ webserver/server.js
@@ -1,13 +1,20 @@
+// Create a web-server using NodeJS
+
+// import 
 import * as net from "net";
 
-
+// when new connection is created
 function newConn(socket) {
+    
+    //log the r-address and r-port
     console.log("new connection", socket.remoteAddress, socket.remotePort); 
 
+    //when end log the message
     server.on('end', () => {
         console.log("EOG. ");
     });
-
+    
+    //log and write the data, if it includes "q", end the connection
     server.on( 'data', (data) => {
         console.log("DATA :", data.toString());
         socket.write(data);
@@ -21,9 +28,14 @@ function newConn(socket) {
 
 }
 
+// initialization
 let server = net.createServer();
+
+//create new connection
 server.on('connection', newConn);
+
+//exception
 server.on('err',  (err) => {throw err});
 
-
+//listen with the host address and port
 server.listen({host: '127.0.0.1', port: 1234});
